1648c95ee79220850c564aa4ba57d5b19ef5ac15
Saurabh shinde <117251508+saurabhshinde18@users.noreply.github.com>
2024-02-06 19:44:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let btn = document.querySelector("button");
+
+btn.addEventListener("click",async()=>
+{
+   let fact=await getFacts();
+   let para=document.querySelector("p");
+    para.innerText=fact;
+})
+
+let url="https://catfact.ninja/fact";
+
+
+async function getFacts(){
+
+    try{
+        let res=await axios.get(url);
+       return  res.data.fact;
+
+    }catch(e){
+        console.log(e);
+    }
+
+}
